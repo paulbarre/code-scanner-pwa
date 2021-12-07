40d7122c3d95645b9f78028ea0fe333b491351f7
Paul Barré <19966190+paulbarre@users.noreply.github.com>
2021-12-07 12:34:53 +0900

    Add script for detector

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,51 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="supported">
+      <div v-if="!isReady">Loading</div>
+      <div v-else>
+        <video ref="video" autoplay></video>
+        <div>Supported formats: {{ detector.supportedFormats.join(', ') }}</div>
+      </div>
+    </div>
+    <div v-else>
+      Sorry. This application is not supported by your device.
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+import Detector from '@/model/Detector';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
+  },
+  data() {
+    return {
+      isReady: false,
+    };
+  },
+  created() {
+    this.supported = Detector.supported;
+    this.detector = new Detector();
+    this.detector.isReady.then(() => {
+      this.isReady = true;
+      this.createVideoStream();
+    });
+  },
+  methods: {
+    async createVideoStream() {
+      const videoStream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+      });
+      console.log(videoStream);
+      if (videoStream) {
+        this.$refs.video.srcObject = videoStream;
+      }
+    },
   },
 };
 </script>
@@ -24,5 +59,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+video {
+  border: 3px solid black;
 }
 </style>
