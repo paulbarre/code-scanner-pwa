a8740c082f5aac4bdb9aad0d7f55247eceab4f73
Paul Barré <19966190+paulbarre@users.noreply.github.com>
2021-12-07 12:35:53 +0900

    Add i18n

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Locale />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Locale from './components/Locale.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Locale,
   },
 };
 </script>
